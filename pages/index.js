61e86066ddd7f6d16e489eb0881d304ed7eac7a9
Bruno Anjos <brunooliveiraanjos@gmail.com>
2023-11-28 23:11:11 +0000

    adicionado .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
     return <h1>Você é a pessoa mais importante pra mim! Te amo! Ah, esta página foi gerada usando uma nova tecnologia que estou aprendendo. ;)</h1>
 }
-
+ 
 export default Home;
