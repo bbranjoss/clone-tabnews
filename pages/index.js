a23b6cd9b2a361352abf5798be99b76487d50610
Bruno Anjos <brunooliveiraanjos@gmail.com>
2023-12-05 23:59:49 +0000

    Texto alterado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é a pessoa mais importante pra mim! Te amo! Ah, esta página foi gerada usando uma nova tecnologia que estou aprendendo. ;)</h1>
+    return <h1>Fala minha Tutuquinha! Eu te amo! Cirei esta página e dediquei esta primeira versão pra você. Estou utilizando uma nova tecnologia que estou aprendendo. ;)</h1>
 }
- 
+
 export default Home;
