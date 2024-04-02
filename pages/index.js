abbfa76186e4fec7bfb4a7cd29d0776854bcef3a
Bruno Anjos <brunooliveiraanjos@gmail.com>
2024-04-02 02:10:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Fala minha Tutuquinha! Eu te amo! To aprendendo umas coisas novas aqui e dediquei esta primeira versão pra você!!! ;)</h1>
+  return (
+    <h1>
+      Fala minha Tutuquinha! Eu te amo! To aprendendo umas coisas novas aqui e
+      dediquei esta primeira versão pra você!!! ;)
+    </h1>
+  );
 }
 
 export default Home;
