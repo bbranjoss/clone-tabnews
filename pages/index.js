44bf90abb39ddd6fb901a7de7c909b0a03cf46b1
Bruno Anjos <brunooliveiraanjos@gmail.com>
2024-06-07 22:37:16 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ function Home() {
   return (
     <h1>
       PROPÓSITO<br />
-      Proteger as crianças das más influências, e como ajudá-las a estar em ambientes saudáveis, protegidas e felizes.<br /><br />
+      Proteger as crianças das más influências, ajudá-las a crescerem em ambientes saudáveis, seguros e felizes.<br /><br />
 
       PÚBLICO-ALVO<br/>
-      Pessoas que gostem de tecnologia e que estejam dispostas a abraçar este propósito tendo como regra de fé e conduta as escrituras sagradas e o ensino de Jesus Cristo "Amar a Deus sobre todas as coisas, e o próximo como a ti mesmo.".
+      Pessoas que que estejam dispostas a abraçar este propósito tendo como regra de fé e conduta as escrituras sagradas e o ensino de Jesus Cristo "Amar a Deus sobre todas as coisas, e o próximo como a ti mesmo."
     </h1>
   );
 }
