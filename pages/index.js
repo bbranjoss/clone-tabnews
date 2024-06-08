de57f0419c7abef9b8a365974f7e33973fc3d006
Bruno Anjos <brunooliveiraanjos@gmail.com>
2024-06-07 22:40:01 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       PROPÓSITO<br />
-      Proteger as crianças das más influências, ajudá-las a crescerem em ambientes saudáveis, seguros e felizes.<br /><br />
+      Proteger as crianças das más influências, ajudando-as a crescerem em ambientes saudáveis, seguros e felizes.<br /><br />
 
       PÚBLICO-ALVO<br/>
       Pessoas que que estejam dispostas a abraçar este propósito, que tem como regra de fé e conduta as escrituras sagradas e procuram viver os dois maiores mandamentos, segundo as palavras de Jesus "Ame a Deus sobre todas as coisas, e o próximo como a ti mesmo."
