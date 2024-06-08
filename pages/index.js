3cf10fcb3a005e85486953bb890b66c55a316e47
Bruno Anjos <brunooliveiraanjos@gmail.com>
2024-06-07 22:39:14 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
       Proteger as crianças das más influências, ajudá-las a crescerem em ambientes saudáveis, seguros e felizes.<br /><br />
 
       PÚBLICO-ALVO<br/>
-      Pessoas que que estejam dispostas a abraçar este propósito tendo como regra de fé e conduta as escrituras sagradas e o ensino de Jesus Cristo "Amar a Deus sobre todas as coisas, e o próximo como a ti mesmo."
+      Pessoas que que estejam dispostas a abraçar este propósito, que tem como regra de fé e conduta as escrituras sagradas e procuram viver os dois maiores mandamentos, segundo as palavras de Jesus "Ame a Deus sobre todas as coisas, e o próximo como a ti mesmo."
     </h1>
   );
 }
