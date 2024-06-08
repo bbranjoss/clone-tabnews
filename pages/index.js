1b3999ceae54802e76e35204177d413e9c10e13b
Bruno Anjos <brunooliveiraanjos@gmail.com>
2024-06-07 22:30:51 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       PROPÓSITO<br />
-      Ajudar missionários<br />
+      Ajudar missionários<br /><br />
 
       PÚBLICO-ALVO<br/>
       Pessoas que gostem de tecnologia e que estejam dispostas a ajudar a propagar o puro evangelho.
