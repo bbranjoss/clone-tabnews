c707daa53a1b8984a0d652c3eba4f9594ef7f4d3
Bruno Anjos <brunooliveiraanjos@gmail.com>
2024-06-07 22:34:57 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ function Home() {
   return (
     <h1>
       PROPÓSITO<br />
-      Ajudar missionários<br /><br />
+      Proteger as crianças das más influências, e como ajudá-las a estar em ambientes saudáveis, protegidas e felizes.<br /><br />
 
       PÚBLICO-ALVO<br/>
-      Pessoas que gostem de tecnologia e que estejam dispostas a ajudar a propagar o puro evangelho.
+      Pessoas que gostem de tecnologia e que estejam dispostas a abraçar este propósito tendo como regra de fé e conduta as escrituras sagradas e o ensino de Jesus Cristo "Amar a Deus sobre todas as coisas, e o próximo como a ti mesmo.".
     </h1>
   );
 }
